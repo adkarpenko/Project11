5b6667b74843fa53b2921b75b69171efaaf38c98
Karpenko Alisa <karpenko-alisa2002@mail.ru>
2018-11-28 23:01:16 +0600

    Добавьте файлы проекта.

---
+++ Project11/Source.cpp
@@ -0,0 +1,441 @@
+#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+
+int main()
+//{
+//	setlocale(LC_ALL, "");
+//
+//	int a, d, q, r;
+//
+//printf("a:");
+//scanf_s("%d",&a);
+//
+//printf("d:");
+//scanf_s("%d",&d);
+//
+// q = (float)a / d;
+//
+//  r =q % d;
+//
+//printf("result: %d\n", q);
+//
+//printf("result: %d\n", r);
+//
+//system("pause");
+//
+//}
+
+//{
+//	setlocale(LC_ALL, "");
+//
+//	int f,x,y;
+//
+//	printf("x:");
+//	scanf_s("%d", &x);
+//
+//	printf("y:");
+//	scanf_s("%d", &y);
+//
+//	if ((x==0)&&(y==0))
+//	{
+//		printf("f: %d\n",f=0 );
+//	}
+//
+//	else if (y==0)
+//	{
+//		printf("f: %d\n",f= 12 / x);
+//	}
+//
+//    else if (x==0)
+//	{
+//		printf("f: %d\n", f=12 / y);
+//	}
+//	else
+//	{
+//		printf("f: %d\n", f=144 / (x*y));
+//	}
+//
+//	system("pause");
+//}
+//
+
+//{
+//	setlocale(LC_ALL, "");
+//
+//	int f, x, y;
+//
+//	printf("x:");
+//	scanf_s("%d", &x);
+//
+//	printf("y:");
+//	scanf_s("%d", &y);
+//
+//	if (x>y)
+//	{
+//		printf("f: %d\n",f= x-y);
+//	}
+//
+//	else
+//	{
+//		printf("f: %d\n", f = y-x);
+//	}
+//
+//	system("pause");
+//}
+
+//{
+//	setlocale(LC_ALL, "");
+//
+//	float f, k, x, y, z, a, m, n;
+//
+//	printf("x:");
+//	scanf_s("%d", &x);
+//
+//	printf("y:");
+//	scanf_s("%d", &y);
+//
+//	printf("z:");
+//	scanf_s("%d", &z);
+//
+//	printf("a:");
+//	scanf_s("%d", &a);
+//
+//	printf("m:");
+//	scanf_s("%d", &m);
+//
+//	printf("n:");
+//	scanf_s("%d", &n);
+//
+//	if (min of x, y, z)
+//	{
+//		printf("f: %f\n", min;
+//	}
+//
+//
+//
+//	system("pause");
+//}
+
+//{
+//	setlocale(LC_ALL,"");
+//
+//	float x, y, z,p;
+//
+//	double m, n, k;
+//
+//	double max, mid, min;
+//
+//	printf("Переменная x: ");
+//
+//	scanf_s("%f\n",&x);
+//
+//	printf("Переменная y: ");
+//	scanf_s("%f\n", &y);
+//
+//	printf("Переменная z: ");
+//	scanf_s("%f\n", &z);
+//
+//	printf("Переменная m: ");
+//
+//	scanf_s("%lf\n", &m);
+//
+//	printf("Переменная n: ");
+//	scanf_s("%lf\n", &n);
+//
+//	printf("Переменная k: ");
+//	scanf_s("%lf\n", &k);
+//
+//	if (x <= 0 && y <= 0 && z <= 0) 
+//	{
+//
+//		printf("Ошибка, значения должны быть больше 0!");
+//	}
+//	
+//
+//	if (k < m && k < n) 
+//	{
+//		
+//		min = k; 
+//	}
+//	
+//	if (n < m && n < k)
+//	{
+//		min = n;
+//	
+//	}
+//	if (m < n && m < k) 
+//	{
+//		min = m; 
+//	}
+//
+//	if (k > m && k > n) 
+//	{
+//		max = k; 
+//	}
+//	
+//	if (n > m && n > k) 
+//	{
+//		max = n; 
+//	}
+//	
+//	if (m > n && m > k) 
+//	{
+//		max = m; 
+//	}
+//
+//	if (m <= n && n <= k) 
+//	{
+//		mid = n; 
+//	}
+//	
+//	else 
+//	{ 
+//		mid = n; 
+//	}
+//	
+//	if (m <= k && k <= n) 
+//	{ 
+//		mid = k; 
+//	}
+//	
+//	else 
+//	{ 
+//		mid = n; 
+//	}
+//
+//	k = min;
+//
+//	m = mid;
+//
+//	n = max;
+//
+//	p = (float) m * n / k;
+//
+//	printf( "result: %f\n",p);
+//
+//	system("pause");
+//}
+
+//{
+//setlocale(LC_ALL, "");
+//
+//int a, b, c;
+//
+//printf("Переменная a: ");
+//scanf_s("%d\n", &a);
+//
+//printf("Переменная b: ");
+//scanf_s("%d\n", &b);
+//
+//printf("Переменная c: ");
+//scanf_s("%d\n", &c);
+//
+//
+//
+//if (a < c && a < b)
+//{
+//
+//	printf("result: %d\n", a);
+//}
+//
+//if (b < c && b < a)
+//{
+//	printf("result: %d\n", b);
+//
+//}
+//if (c < b && c < a)
+//{
+//	printf("result: %d\n", c);
+//}
+//
+//
+//
+//system("pause");
+//}
+
+//{
+//setlocale(LC_ALL, "");
+//
+//int a, b, c;
+//
+//printf("Переменная a: ");
+//scanf_s("%d", &a);
+//
+//printf("Переменная b: ");
+//scanf_s("%d", &b);
+//
+//printf("Переменная c: ");
+//scanf_s("%d", &c);
+//
+//
+//
+//if (a > c && a > b)
+//{
+//
+//	printf("result: %d\n", a);
+//}
+//
+//if (b > c && b > a)
+//{
+//	printf("result: %d\n", b);
+//
+//}
+//
+//if (c > b && c > a)
+//{
+//	printf("result: %d\n", c);
+//}
+//
+//system("pause");
+//}
+
+//{
+//setlocale(LC_ALL, "");
+//
+//int a, b, c;
+//
+//printf("Переменная a: ");
+//scanf_s("%d", &a);
+//
+//printf("Переменная b: ");
+//scanf_s("%d", &b);
+//
+//printf("Переменная c: ");
+//scanf_s("%d", &c);
+//
+//
+//
+//if (a > c && a > b)
+//{
+//
+//	printf("result: %d\n", a);
+//}
+//
+//if (b > c && b > a)
+//{
+//	printf("result: %d\n", b);
+//
+//}
+//
+//if (c > b && c > a)
+//{
+//	printf("result: %d\n", c);
+//}
+//
+//system("pause");
+//}
+
+//{
+//setlocale(LC_ALL, "");
+//
+//int x, y, z;
+//
+//printf("Переменная x: ");
+//scanf_s("%d", &x);
+//
+//printf("Переменная y: ");
+//scanf_s("%d", &y);
+//
+//printf("Переменная z: ");
+//scanf_s("%d", &z);
+//
+//
+//if (pow(x, 2) + pow(y, 2) ==pow(z, 2))
+//
+//{
+//
+//	printf("True\n");
+//}
+//
+//else
+//{
+//	printf("False\n");
+//}
+//
+//system("pause");
+//}
+
+//{
+//setlocale(LC_ALL, "");
+//
+//int x, y;
+//
+//printf("Переменная x: ");
+//scanf_s("%d", &x);
+//
+//printf("Переменная y: ");
+//scanf_s("%d", &y);
+//
+//
+//if (x<0 && y<0)
+//
+//{
+//  printf("no solutions \n");
+//}
+//
+//else if (x % y == 0 || y % x == 0)
+//{
+//	printf("true \n");
+//}
+//
+//else
+//{
+//	printf("false \n");
+//}
+//
+//system("pause");
+//}
+
+//{
+//	setlocale(LC_ALL, "");
+//
+//	int x, y, z;
+//
+//	printf("Переменная x: ");
+//	scanf_s("%d", &x);
+//
+//	printf("Переменная y: ");
+//	scanf_s("%d", &y);
+//
+//	printf("Переменная z: ");
+//	scanf_s("%d", &z);
+//
+//
+//	if (x < 0 && y < 0 && z < 0)
+//
+//	{
+//		printf("no solutions \n");
+//	}
+//
+//	else if (x > z && x > y && z + y > x)
+//	{
+//		printf("true \n");
+//	}
+//
+//	else
+//	{
+//		printf("false \n");
+//	}
+//
+//	if (y > z && y > x && x + z > y)
+//	{
+//		printf("true \n");
+//	}
+//	else
+//	{
+//		printf("false \n");
+//	}
+//
+//	if (z > x && z > y && x + y > z)
+//	{
+//		printf("true \n");
+//	}
+//	else
+//	{
+//		printf("false \n");
+//	}
+//
+//	system("pause");
+//}
